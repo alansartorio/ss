c6e4cb9299eb061650ad41fa9ba47f57302e21a0
Alan Sartorio <alan42ga@hotmail.com>
2023-05-16 20:54:02 -0300

    feat(mesh): implement drawing

--- mesh/src/main.rs
+++ mesh/src/main.rs
@@ -1,9 +1,7 @@
 use capturable_visualization::VisualizationBuilder;
-use nannou::{
-    prelude::{self, Update},
-    App, Draw,
-};
-use ndarray::Array2;
+use itertools::Itertools;
+use nannou::{color, prelude::*, App, Draw};
+use ndarray::{Array2, Axis};
 
 const K: f64 = 10e4;
 
@@ -26,6 +24,7 @@ struct MovingNode {
     weight: f64,
 }
 
+#[derive(Debug)]
 enum Node {
     Moving(MovingNode),
     Fixed { position: Vector2 },
@@ -66,10 +65,31 @@ fn draw(_app: &App, model: &Model, draw: &Draw) {
     for node in &model.mesh_nodes {
         let position = match node {
             Node::Moving(MovingNode { position, .. }) | Node::Fixed { position } => position,
-        };
-        draw.ellipse()
-            .radius(0.05)
-            .x(position.x as f32)
-            .y(position.y as f32);
+        }
+        .cast();
+        draw.ellipse().radius(0.05).x(position.x).y(position.y);
+    }
+
+    for row in model
+        .mesh_nodes
+        .axis_iter(Axis(0))
+        .chain(model.mesh_nodes.axis_iter(Axis(1)))
+    {
+        for (node1, node2) in row.iter().tuple_windows() {
+            let position1 = match node1 {
+                Node::Moving(MovingNode { position, .. }) | Node::Fixed { position } => position,
+            }
+            .cast();
+            let position2 = match node2 {
+                Node::Moving(MovingNode { position, .. }) | Node::Fixed { position } => position,
+            }
+            .cast();
+
+            draw.line()
+                .start(pt2(position1.x, position1.y))
+                .end(pt2(position2.x, position2.y))
+                .stroke_weight(0.02)
+                .color(color::WHITE);
+        }
     }
 }
