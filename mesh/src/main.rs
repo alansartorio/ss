e3cca479559cfc3e8f780548877e1d22976d7c04
Alan Sartorio <alan42ga@hotmail.com>
2023-05-16 21:59:35 -0300

    feat(mesh): add mesh update loop with gear predictor corrector

--- mesh/src/main.rs
+++ mesh/src/main.rs
@@ -1,7 +1,8 @@
 use capturable_visualization::VisualizationBuilder;
+use gear_predictor_corrector::{GearCorrector, GearPredictor};
 use itertools::Itertools;
 use nannou::{color, prelude::*, App, Draw};
-use ndarray::{Array2, Axis};
+use ndarray::{s, Array2, Axis};
 
 const K: f64 = 10e4;
 
@@ -15,6 +16,8 @@ fn link_force(position1: Vector2, position2: Vector2) -> f64 {
         0.0
     }
 }
+
+const GRAVITY: f64 = 9.8;
 
 #[derive(Debug, Default)]
 struct MovingNode {
@@ -43,7 +46,7 @@ struct Model {
 
 fn model(app: &App) -> Model {
     let mesh_nodes = Array2::<Node>::from_shape_fn((11, 11), |(y, x)| {
-        let position = Vector2::new(x as f64, y as f64);
+        let position = Vector2::new(x as f64, y as f64 + 3.5);
         if y == 10 {
             Node::Fixed { position }
         } else {
@@ -58,10 +61,70 @@ fn model(app: &App) -> Model {
     Model { mesh_nodes }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+fn update(_app: &App, model: &mut Model, update: Update) {
+    let dt = update.since_last.as_secs_f64();
+    for node in model.mesh_nodes.iter_mut() {
+        match node {
+            Node::Moving(MovingNode {
+                position,
+                velocity,
+                higher_order,
+                ..
+            }) => {
+                let predictions = GearPredictor {
+                    rs: [
+                        *position,
+                        *velocity,
+                        higher_order[0],
+                        higher_order[1],
+                        higher_order[2],
+                        higher_order[3],
+                    ],
+                }
+                .predict(dt)
+                .predictions;
+                *position = predictions[0];
+                *velocity = predictions[1];
+                higher_order.copy_from_slice(&predictions[2..]);
+            }
+            Node::Fixed { .. } => (),
+        };
+    }
+    let mut accelerations = Array2::<Vector2>::zeros(model.mesh_nodes.dim());
+    for ((y, x), node) in model.mesh_nodes.indexed_iter() {
+        accelerations[[y, x]] += Vector2::new(0.0, -GRAVITY);
+    }
+    model
+        .mesh_nodes
+        .zip_mut_with(&accelerations, |mut node, acceleration| {
+            if let Node::Moving(MovingNode {
+                position,
+                velocity,
+                higher_order,
+                ..
+            }) = &mut node
+            {
+                let corrected = GearCorrector {
+                    predictions: [
+                        *position,
+                        *velocity,
+                        higher_order[0],
+                        higher_order[1],
+                        higher_order[2],
+                        higher_order[3],
+                    ],
+                }
+                .correct(*acceleration, dt);
+                *position = corrected[0];
+                *velocity = corrected[1];
+                higher_order.copy_from_slice(&corrected[2..]);
+            }
+        });
+}
 
 fn draw(_app: &App, model: &Model, draw: &Draw) {
-    let draw = draw.scale(0.1);
+    draw.background().color(BLACK);
+    let draw = draw.x(0.5).scale(1.0 / 14.0).x(-5.0);
     for node in &model.mesh_nodes {
         let position = match node {
             Node::Moving(MovingNode { position, .. }) | Node::Fixed { position } => position,
