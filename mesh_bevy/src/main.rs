1f270c73456ee8d6250f2e156be4bf9fd06bcbc8
Alan Sartorio <alan42ga@hotmail.com>
2023-05-17 23:10:18 -0300

    feat(mesh_bevy): add fixed nodes

--- mesh_bevy/src/main.rs
+++ mesh_bevy/src/main.rs
@@ -2,7 +2,7 @@ use bevy::math::{vec2, vec3, Vec3Swizzles};
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 
-#[derive(Component)]
+#[derive(Component, PartialEq, Eq)]
 enum Node {
     Fixed,
     Moving,
@@ -16,7 +16,7 @@ fn generate_grid_mesh(
     end: Vec2,
     columns: usize,
     rows: usize,
-) -> (Vec<Vec2>, Vec<(usize, usize)>) {
+) -> (Vec<(Vec2, Node)>, Vec<(usize, usize)>) {
     let diagonal = (end - start) / vec2((columns - 1) as f32, (rows - 1) as f32);
     let mut nodes = vec![];
     let mut edges = vec![];
@@ -24,7 +24,7 @@ fn generate_grid_mesh(
         let row_index = y * columns;
         for x in 0..columns {
             let position = start + vec2(diagonal.x * x as f32, diagonal.y * y as f32);
-            nodes.push(position);
+            nodes.push((position, if y == 0 { Node::Fixed } else { Node::Moving }));
         }
         for x in 1..columns {
             edges.push((row_index + x - 1, row_index + x));
@@ -44,7 +44,7 @@ fn add_nodes(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-let (nodes, edges) = generate_grid_mesh(vec2(-0.05, 0.1), vec2(0.05, 0.0), 10, 10);
+    let (nodes, edges) = generate_grid_mesh(vec2(-0.05, 0.1), vec2(0.05, 0.0), 10, 10);
 
     let mut add_node = |pos: Vec2, node_type: Node| {
         commands
@@ -64,17 +64,15 @@ let (nodes, edges) = generate_grid_mesh(vec2(-0.05, 0.1), vec2(0.05, 0.0), 10, 1
     };
 
     let mut node_entities = vec![];
-    for node in nodes {
-        node_entities.push(add_node(node, Node::Moving));
+    for (pos, node_type) in nodes {
+        node_entities.push(add_node(pos, node_type));
     }
 
     let mut add_edge = |start, end| {
         commands
             .spawn((
                 MaterialMesh2dBundle {
-                    mesh: meshes
-                        .add(shape::Quad::new(vec2(1.0, 0.001)).into())
-                        .into(),
+                    mesh: meshes.add(shape::Quad::new(vec2(1.0, 0.001)).into()).into(),
                     material: materials.add(ColorMaterial::from(Color::BLACK)),
                     transform: Transform {
                         translation: vec3(0.0, 0.0, 0.4),
@@ -102,6 +100,14 @@ fn add_camera(mut commands: Commands) {
     });
 }
 
+fn update_nodes(time: Res<Time>, mut nodes: Query<(&Node, &mut Transform)>) {
+    for (node, mut transform) in nodes.iter_mut() {
+        if *node == Node::Moving {
+            transform.translation.y -= time.delta_seconds() * 0.01;
+        }
+    }
+}
+
 fn update_edges(
     nodes: Query<&Transform, With<Node>>,
     mut edges: Query<(&Edge, &mut Transform), Without<Node>>,
@@ -123,6 +129,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, add_camera)
         .add_systems(Startup, add_nodes)
-        .add_systems(Update, update_edges)
+        .add_systems(Update, (update_nodes, update_edges.after(update_nodes)))
         .run();
 }
